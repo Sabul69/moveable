fbd6a6f9c0267b829de2e0e58b99767dd77d960e
Saul Hernandez <saul_skillet@hotmail.com>
2023-02-22 22:20:48 -0600

    final

--- src/App.js
+++ src/App.js
@@ -110,7 +110,6 @@ const Component = ({
   id,
   setSelected,
   isSelected = false,
-  updateEnd,
 }) => {
   const ref = useRef();
   const [show, setShow] = useState(false);
@@ -129,6 +128,7 @@ const Component = ({
   let parentBounds = parent?.getBoundingClientRect();
 
   const onResize = async (e) => {
+    console.log(e);
     // ACTUALIZAR ALTO Y ANCHO
     let newWidth = e.width;
     let newHeight = e.height;
@@ -170,41 +170,10 @@ const Component = ({
     });
   };
 
-  const onResizeEnd = async (e) => {
-    let newWidth = e.lastEvent?.width;
-    let newHeight = e.lastEvent?.height;
-
-    const positionMaxTop = top + newHeight;
-    const positionMaxLeft = left + newWidth;
-
-    if (positionMaxTop > parentBounds?.height)
-      newHeight = parentBounds?.height - top;
-    if (positionMaxLeft > parentBounds?.width)
-      newWidth = parentBounds?.width - left;
-
-    const { lastEvent } = e;
-    const { drag } = lastEvent;
-    const { beforeTranslate } = drag;
-
-    const absoluteTop = top + beforeTranslate[1];
-    const absoluteLeft = left + beforeTranslate[0];
-
-    updateMoveable(
-      id,
-      {
-        top: absoluteTop,
-        left: absoluteLeft,
-        width: newWidth,
-        height: newHeight,
-        color,
-        bg,
-      },
-      true
-    );
-  };
+  const onResizeEnd = async (e) => {};
 
   return (
-    <div>
+    <>
       <div
         ref={ref}
         className="draggable shadow"
@@ -225,32 +194,6 @@ const Component = ({
         onMouseOut={() => setShow(false)}
         onMouseOver={() => setShow(true)}
       >
-        <Moveable
-          target={isSelected && ref.current}
-          resizable
-          draggable
-          onDrag={(e) => {
-            updateMoveable(id, {
-              top: e.top,
-              left: e.left,
-              width,
-              height,
-              color,
-            });
-          }}
-          onResize={onResize}
-          onResizeEnd={onResizeEnd}
-          keepRatio={false}
-          throttleResize={1}
-          renderDirections={["nw", "n", "ne", "w", "e", "sw", "s", "se"]}
-          edge={false}
-          zoom={1}
-          origin={false}
-          padding={{ left: 0, top: 0, right: 0, bottom: 0 }}
-          snappable={true}
-          verticalGuidelines={[10, 20, 50]}
-          horizontalGuidelines={[10, 20, 50]}
-        />
         {show && (
           <button
             className="del"
@@ -262,6 +205,32 @@ const Component = ({
           </button>
         )}
       </div>
-    </div>
+      <Moveable
+        target={isSelected && ref.current}
+        resizable
+        draggable
+        onDrag={(e) => {
+          updateMoveable(id, {
+            top: e.top,
+            left: e.left,
+            width,
+            height,
+            color,
+          });
+        }}
+        onResize={onResize}
+        onResizeEnd={onResizeEnd}
+        keepRatio={false}
+        throttleResize={1}
+        renderDirections={["nw", "n", "ne", "w", "e", "sw", "s", "se"]}
+        edge={false}
+        zoom={1}
+        origin={false}
+        padding={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        snappable={true}
+        verticalGuidelines={[10, 20, 50]}
+        horizontalGuidelines={[10, 20, 50]}
+      />
+    </>
   );
 };
